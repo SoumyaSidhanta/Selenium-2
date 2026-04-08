88a1f7d2efe3867a39fe30bd6d11f693261e946d
Soumya_Sidhanta_Mohanta <163277537+SoumyaSidhanta@users.noreply.github.com>
2026-04-08 11:25:40 +0530

    Practice Relative Locators

---
+++ src/main/java/SeleniumJava/SeleniumJava/RealtiveLocators.java
@@ -0,0 +1,24 @@
+package SeleniumJava.SeleniumJava;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static  org.openqa.selenium.support.locators.RelativeLocator.*;
+
+public class RealtiveLocators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("web driver.chrome.driver", "C:\\Users\\mohan\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+        
+        WebElement nameBox = driver.findElement(By.cssSelector("[name='name']"));
+        
+        System.out.println(driver.findElement(with(By.tagName("label")).above(nameBox)).getText());
+	}
+
+}
