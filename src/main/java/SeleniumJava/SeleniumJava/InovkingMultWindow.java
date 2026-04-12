3f2159f7e66f17efdf0101e885d34567b6c0c10e
Soumya_Sidhanta_Mohanta <163277537+SoumyaSidhanta@users.noreply.github.com>
2026-04-12 12:19:27 +0530

    Changing Tabs

--- src/main/java/SeleniumJava/SeleniumJava/InovkingMultWindow.java
+++ src/main/java/SeleniumJava/SeleniumJava/InovkingMultWindow.java
@@ -3,13 +3,16 @@ package SeleniumJava.SeleniumJava;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class InovkingMultWindow {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
 		System.setProperty("web driver.chrome.driver", "C:\\Users\\mohan\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
@@ -23,7 +26,32 @@ public class InovkingMultWindow {
         String parentWindowId= iterators.next();
         String childWindowId= iterators.next();
         driver.switchTo().window(childWindowId);
+        
         driver.get("https://rahulshettyacademy.com/");
+        
+        JavascriptExecutor js =(JavascriptExecutor)driver;
+	    js.executeScript("window.scrollBy(0,500)");
+	    
+        driver.findElement(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/courses']")).click();
+        
+        Set<String> newHandle= driver.getWindowHandles();
+        Iterator<String> it = newHandle.iterator();
+        String parentId1= it.next();
+        String childId2= it.next();
+        String childId3= it.next();
+        driver.switchTo().window(childId3);
+        
+        js.executeScript("window.scrollBy(0,2500)");
+        
+        String ch3=  driver.findElement(By.xpath("//h2[contains(text(),'Core Java for Automation Testers + Interview Progr')]")).getText();
+        System.out.println(ch3);
+
+        Thread.sleep(3000);
+   
+        driver.switchTo().window(parentId1);
+        
+        driver.findElement(By.cssSelector("[name='name']")).sendKeys(ch3);
+        
         }
 
 }
